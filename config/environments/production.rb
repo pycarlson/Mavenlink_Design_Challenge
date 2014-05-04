f6e57fdd26910a120ca6b4b229799541bd279415
Paulette Luftig <pyluftig@gmail.com>
2014-05-04 12:28:56 -0700

    add to config file to get heroku to load assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Mavenlink::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Generate digests for assets URLs.
   config.assets.digest = true
